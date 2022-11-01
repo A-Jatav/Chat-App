0b9ec9adad4df4295684d018c04a3f262ee2ffd1
A-Jatav <108002072+A-Jatav@users.noreply.github.com>
2022-11-01 20:02:25 +0530

    Add files via upload

---
+++ primary.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyC2UsNFRKkYbbaf8eoCZiogajfBYf_nRX4",
+    authDomain: "chat-app-a8f5b.firebaseapp.com",
+    databaseURL: "https://chat-app-a8f5b-default-rtdb.firebaseio.com",
+    projectId: "chat-app-a8f5b",
+    storageBucket: "chat-app-a8f5b.appspot.com",
+    messagingSenderId: "36525248009",
+    appId: "1:36525248009:web:50a2116cc8debe5a09b2d8"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
